d0ebbe506d108b51969153af695b91b4ec287941
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-16 06:46:17 +0530

    Update terraform/terraform-learning/

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,48 @@ variable "example" {
 output "example" {
   value = var.example
 }
+
+## Data types
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+
+### Variable Types
+variable "sample4" {
+  type = list(string) ## optional
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sample5" {
+  type = map(string) ## optional
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+output "sample4-1" {
+  value = var.sample4[0]
+}
+
+output "sample5-1" {
+  value = var.sample5["number"]
+}
