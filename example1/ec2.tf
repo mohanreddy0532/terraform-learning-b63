eac1b130a0a5257cd545700f5dd0a4f5c8fc5dfe
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-16 07:13:44 +0530

    Update terraform/terraform-learning/

---
+++ example1/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "sample" {
+  ami  = "ami-0bb6af715826253bf"
+  type = "t3.micro"
+}
+
+output "public_ip" {
+  value = aws_instance.sample.public_ip
+}
