785998b4859aa7d93f4a85c20e2c54ace1bed3e7
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-18 07:04:49 +0530

    Update terraform/terraform-learning/

--- example2/ec2/data.tf
+++ example2/ec2/data.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "example" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "base-with-ansible"
-  owners           = ["self"]
+  most_recent = true
+  name_regex  = "base-with-ansible"
+  owners      = ["self"]
 }
