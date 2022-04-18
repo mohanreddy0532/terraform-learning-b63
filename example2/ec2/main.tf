2b5ed21792c578eadbea027163f7827555d91c1c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-18 07:12:18 +0530

    Update terraform/terraform-learning/

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -2,6 +2,14 @@ resource "aws_instance" "sample" {
   ami                    = data.aws_ami.example.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg]
+  provisioner "local-exec" {
+    command = <<EOF
+sleep 60
+cd /home/centos/ansible
+ansible-playbook -i ${self.public_ip}, roboshop.yml -e HOST=all -e role_name=frontend -e ENV=dev
+EOF
+
+  }
 }
 
 variable "sg" {}
