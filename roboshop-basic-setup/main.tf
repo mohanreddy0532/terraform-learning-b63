438c44f43f17489a98b923d8168ca03ef21611e0
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-19 06:51:03 +0530

    Update terraform/terraform-learning/

--- roboshop-basic-setup/main.tf
+++ roboshop-basic-setup/main.tf
@@ -1,7 +1,17 @@
-module "component" {
-  count       = length(var.COMPONENTS)
-  source      = "./ec2"
-  COMPONENT   = var.COMPONENTS[count.index]
-  APP_VERSION = var.APP_VERSION[count.index]
-}
+//module "component" {
+//  count       = length(var.COMPONENTS)
+//  source      = "./ec2"
+//  COMPONENT   = var.COMPONENTS[count.index]
+//  APP_VERSION = var.APP_VERSION[count.index]
+//}
+//
 
+
+resource "null_resource" "sample" {
+  count = length(var.ALL_COMPONENTS)
+  provisioner "local-exec" {
+    command = <<EOF
+echo ${var.ALL_COMPONENTS[0]}
+EOF
+  }
+}
--- roboshop-basic-setup/vars.tf
+++ roboshop-basic-setup/vars.tf
@@ -1,7 +1,7 @@
 variable "COMPONENTS" {
   default = [
     "mongodb",
-    "catalogue"
+    "catalogue",
   ]
 }
 
@@ -9,5 +9,18 @@ variable "APP_VERSION" {
   default = [
     "null",
     "1.0.0"
+  ]
+}
+
+variable "ALL_COMPONENTS" {
+  default = [
+    {
+      COMPONENT   = "mongodb",
+      APP_VERSION = "null"
+    },
+    {
+      COMPONENT   = "catalogue",
+      APP_VERSION = "1.0.0"
+    }
   ]
 }
