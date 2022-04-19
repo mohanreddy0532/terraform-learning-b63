470824275be406cc03b7ff9f85d4b90e4012d2e6
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-19 07:15:31 +0530

    Update terraform/terraform-learning/

--- roboshop-basic-setup/main.tf
+++ roboshop-basic-setup/main.tf
@@ -14,7 +14,7 @@ resource "null_resource" "sample" {
   count = length(var.ALL_COMPONENTS)
   provisioner "local-exec" {
     command = <<EOF
-echo COMPONENT = ${[for o in var.ALL_COMPONENTS[count.index] : o.COMPONENT]}
+echo COMPONENT = ${for o in var.ALL_COMPONENTS[count.index] : o.COMPONENT}
 EOF
   }
 }
