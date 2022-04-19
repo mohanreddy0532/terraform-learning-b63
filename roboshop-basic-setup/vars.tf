01dbdd88e574f74d646a81cd735ad20054463efc
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-19 07:26:34 +0530

    Update terraform/terraform-learning/

--- roboshop-basic-setup/vars.tf
+++ roboshop-basic-setup/vars.tf
@@ -16,5 +16,13 @@ variable "ALL_COMPONENTS" {
   default = {
     mongodb   = "null"
     catalogue = "1.0.0"
+    redis     = "null"
+    user      = "1.0.0"
+    cart      = "1.0.2"
+    mysql     = "null"
+    shipping  = "1.0.0"
+    rabbitmq  = "null"
+    payment   = "1.0.0"
+    frontend  = "1.0.0"
   }
 }
