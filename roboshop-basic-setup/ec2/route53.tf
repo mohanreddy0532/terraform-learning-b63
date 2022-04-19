9edbac0821668e9a51d83b6481cba04b210c5fb3
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-19 06:23:48 +0530

    Update terraform/terraform-learning/

--- roboshop-basic-setup/ec2/route53.tf
+++ roboshop-basic-setup/ec2/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "component" {
   zone_id = "Z08005452NVNO9YZAOL1R"
-  name    = "${var.COMPONENT}.roboshop.internal"
+  name    = "${var.COMPONENT}-dev.roboshop.internal"
   type    = "A"
   ttl     = "300"
   records = [aws_spot_instance_request.cheap_worker.private_ip]
